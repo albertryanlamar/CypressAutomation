23b5ff328507d6cd9071f0960e6231049e67d83f
albertryanlamar@gmail.com <albertryanlamar@gmail.com>
2023-10-11 21:25:29 +0800

    Save this

--- cypress/e2e/Sample_API_Testing/api_test/Day7Authentications.cy.js
+++ cypress/e2e/Sample_API_Testing/api_test/Day7Authentications.cy.js
@@ -1,4 +1,4 @@
-const tkn = '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';
+const tkn = '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';
 
 describe("Authentication",()=>{
 
@@ -30,6 +30,18 @@ describe("Authentication",()=>{
         reqApikeyauth().then((response)=>{
             vldApikeyauth(response);
         });
+    })
+
+    it("OAuth2:Get oauth2 accessToken",()=>{
+        oauthToken().then((response)=>{
+            vld_oauthToken(response);
+        })
+    })
+
+    it("OAuth2:request",()=>{
+        oauth2token().then((response)=>{
+        vld_oauth2token(response);
+        })
     })
 
 })
@@ -100,5 +112,54 @@ let reqApikeyauth = ()=>{
 }
 let vldApikeyauth = (response)=>{
     expect(response.status).to.equal(200);
+
+}
+
+/* Oauth token 
+1) Get the Oauth2 access token
+POST:https://github.com/login/oauth/access_token
+Query Params
+        -------------
+        client_id
+        client_secrete
+        code
+2) Send GET request by using access token.
+https://api.github.com/user/repos
+auth:accessToken
+*/
+let oauthToken =()=>
+{
+    return cy.request({
+        method:'POST',
+        url:'https://github.com/login/oauth/access_token',
+        qs:{
+            client_id:'dc8277a12bfb2092a571',
+            client_secret:'4f04e4fc90a5eca5f754bbd6227304ad46393d61',
+            code:'f627adc7a044c356abdf'
+        }
+    })
+}
+let accessToken;
+
+let vld_oauthToken = (response)=>{
+    expect(response.status).to.equal(200);
+    // access_token=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&scope=&token_type=bearer
+    const param = response.body.split('&');
+    accessToken = param[0].split("=")[1];
+}
+
+let oauth2token = ()=>
+{
+   return cy.request({
+        method:'GET',
+        url:'https://api.github.com/user/repos',
+        headers:{
+            'Authorization':'Bearer '+accessToken
+        }
+    })
+}
+let vld_oauth2token = (response)=>{
+    expect(response.status).to.equal(200);
+    expect(response.body[0].owner.login).to.equal("albertryanlamar");
 
 }
